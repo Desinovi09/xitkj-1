7c97f64851aed59bf4055f11a0a45c0125386bbf
Desi Novita Sari <desinovita09tkj1@gmail.com>
2016-09-20 21:10:02 -0700

    Add files via upload

---
+++ ARDUINO.ino
@@ -0,0 +1,15 @@
+//DESI NOVITA SARI
+//XI TKJ1
+void setup() {
+  // put your setup code here, to run once:
+ //Memberi target pin uotput yang digunakan
+ pinMode (10,OUTPUT) ;
+}
+
+void loop() {
+  //Memberi perulangan
+digitalWrite (10,HIGH); //pada pin 10, LED menyala
+delay(5000); //Tunda 2 sec pada saat nyala
+digitalWrite(10,LOW); //Pada pin 10, LED mati
+delay(5000); //Tunda 2 sec pada saat mati
+}
